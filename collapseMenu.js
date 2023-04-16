afa059d1d03bfc28cec3c08cb5d2dc49e9ed12ec
Andres Barroso <andres.barroso@gmail.com>
2023-04-16 03:24:53 -0400

    added simple navigation menu

--- collapseMenu.js
+++ collapseMenu.js
@@ -1,28 +1,31 @@
-const collapse = document.getElementsByClassName("collapse-button");
-const header = document.querySelector("header");
+const collapse = document.getElementsByClassName("collapse");
+const header = document.getElementById("header");
 const barsIcon = document.getElementById("bars-icon");
 const crossIcon = document.getElementById("cross-icon");
+const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
 const hide = (el) => el.style.setProperty("display", "none");
 const show = (el) => el.style.setProperty("display", "block");
-const noHeight = (el) => el.style.setProperty("height", "0px");
-const fullHeight = (el) => el.style.setProperty("height", `${el.scrollHeight}px`);
-
+const noHeight = (el) => el.style.setProperty("height", "0")
+const normalHeight = (el) => el.style.setProperty("height", `${crossIcon.scrollHeight}px`);
+const fullHeight = (el) => el.style.setProperty("height", `${barsIcon.scrollHeight+(rem*11.938)}px`);
+const backgroundColor = (el) => el.style.setProperty("background-color", "#FFE376");
+const noBackgroundColor = (el) => el.style.setProperty("background-color", "#FFBD00");
 
 for (let i = 0; i < collapse.length; i++) {
   collapse[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    header.classList.toggle("active");
-    let navigationMenu = this.nextElementSibling;
-    if (navigationMenu.style.height !== "" && navigationMenu.style.height !== "0px") {
-      console.log("navigationMenu.style.height: close", navigationMenu.style.height)
+    let navigation = this.nextElementSibling;
+    if (header.style.height !== "" && header.style.height !== barsIcon.scrollHeight + "px") {
+      noHeight(navigation);
+      normalHeight(header);
+      noBackgroundColor(header)
       show(barsIcon);
       hide(crossIcon);
-      noHeight(navigationMenu);
     } else {
-      console.log("navigationMenu.style.height: opens", navigationMenu.style.height)
+      fullHeight(header);
+      backgroundColor(header)
       hide(barsIcon);
       show(crossIcon);
-      fullHeight(navigationMenu);
+      fullHeight(navigation);
     }
   });
 }
