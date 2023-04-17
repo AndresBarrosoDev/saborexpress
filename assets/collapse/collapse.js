b29ca14a95c62abb22a89c2dedbd1c8499867031
Andres Barroso <andres.barroso@gmail.com>
2023-04-17 01:19:22 -0400

    improved the navigation menu

--- assets/collapse/collapse.js
+++ assets/collapse/collapse.js
@@ -1,30 +1,35 @@
+// Menu button
+document.addEventListener("DOMContentLoaded", function() {
+    let navIcon = document.querySelectorAll("#nav-icon");
+    for (var i = 0; i < navIcon.length; i++) {
+      navIcon[i].addEventListener("click", function() {
+        this.classList.toggle("open");
+      });
+    }
+  });
+
+// Collapse navigation
 const collapse = document.getElementsByClassName("collapse");
 const header = document.getElementById("header");
-const barsIcon = document.getElementById("bars-icon");
-const crossIcon = document.getElementById("cross-icon");
 const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
 const hide = (el) => el.style.setProperty("display", "none");
 const show = (el) => el.style.setProperty("display", "block");
 const noHeight = (el) => el.style.setProperty("height", "0")
-const normalHeight = (el) => el.style.setProperty("height", `${crossIcon.scrollHeight}px`);
-const fullHeight = (el) => el.style.setProperty("height", `${barsIcon.scrollHeight+(rem*11.938)}px`);
+const normalHeight = (el) => el.style.setProperty("height", `${4.125*rem}px`);
+const fullHeight = (el) => el.style.setProperty("height", `${(4.125*rem)+(rem*10)}px`);
 const backgroundColor = (el) => el.style.setProperty("background-color", "#FFE376");
 const noBackgroundColor = (el) => el.style.setProperty("background-color", "#FFBD00");
 
 for (let i = 0; i < collapse.length; i++) {
   collapse[i].addEventListener("click", function () {
     let navigation = this.nextElementSibling;
-    if (header.style.height !== "" && header.style.height !== barsIcon.scrollHeight + "px") {
+    if (header.style.height !== "" && header.style.height !== (4.125*rem) + "px") {
       noHeight(navigation);
       normalHeight(header);
       noBackgroundColor(header)
-      show(barsIcon);
-      hide(crossIcon);
     } else {
       fullHeight(header);
       backgroundColor(header)
-      hide(barsIcon);
-      show(crossIcon);
       fullHeight(navigation);
     }
   });
--- assets/hamburger/hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let navIcon = document.querySelectorAll("#nav-icon4");
-    for (var i = 0; i < navIcon.length; i++) {
-      navIcon[i].addEventListener("click", function() {
-        this.classList.toggle("open");
-      });
-    }
-  });
